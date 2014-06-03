4a7ff80db48e294359ed3d9ed3b238f08ccb978a
Ajeey <ajey.charantimath@gmail.com>
2014-06-03 16:07:16 +0530

    ember normal user list and edit & save in object

--- js/app.js
+++ js/app.js
@@ -1,102 +1,75 @@
 App = Ember.Application.create();
 
-/*App.Router.map(function() {
-  // put your routes here
-});
-
-App.IndexRoute = Ember.Route.extend({
-  model: function() {
-    return ['red', 'yellow', 'blue'];
-  }
-});*/
-
-App.Router.map(function(){
-  this.resource('about');
-  this.resource('usage');
-  this.resource('posts', function(){
-    this.resource('post', { path: '/:post_id' });
-  });
-
-});
-
-App.PostsRoute = Ember.Route.extend({
-  /*model: function(){
-    return posts;
-  }*/
+App.ApplicationRoute = Ember.Route.extend({
   model: function(){
-    return $.getJSON('http://tomdale.net/api/get_recent_posts/?callback=?').then(function(data){
-      return data.posts.map(function(post){
-        post.body = post.content;
-        return post;
-      });
-    });
-  }
-});
+    return users;
+  },
 
-App.UsageRoute = Ember.Route.extend({
-  model: function() {
-    return $.getJSON('api.coursehub.tv/user/usage?ltiKey=Jgs0wJjDymf4DrFhROa6579NvmTCsEw1lfsYpScd47Y7odf5s62o421qeLsMQfE1&ltiUserId=6378&ltiContextId=185', 
-      function(data) {
-        
-    });
-    console.log("test");
-  }
-});
-
-
-App.PostRoute = Ember.Route.extend({
-  /*model: function(params) {
-    return posts.findBy('id', params.post_id);
-  }*/
-  model: function(params) {
-    return $.getJSON('http://tomdale.net/api/get_post/?id='+params.post_id+'&callback=?').then(function(data){
-      return data.posts.map(function(post){
-        post.body = post.content;
-        return post;
-      });
-    });
-  }
-});
-
-App.PostController = Ember.ObjectController.extend({
-  isEditing: false,
-
-  actions: {
-    edit: function(){
-      this.set('isEditing', true);
-    },
-
-    doneEditing: function(){
-      this.set('isEditing', false);
+  events: {
+    createUser: function() {
+      var users = this.modelFor('application');
+       var user = users.pushObject({id: users.length});
+      this.transitionTo('editUser', user);
     }
   }
 
+  // called when the user enters this route
+  /*activate: function() {
+    this.interval = setInterval(function(){
+      var timer = this.get('controller.model.timer');
+      this.set('controller.model.timer', timer + 1);
+    }.bind(this), 1000);
+  },
+
+  // gets called when the user leaves the route
+  deactivate: function(){
+    clearInterval(this.interval);
+  }*/
+
+});
+
+App.Router.map(function(){
+  this.resource('user', { path: 'users/:user_id'});
+  this.resource('editUser', { path: 'users/:user_id/edit' });
+});
+
+App.UserRoute = Ember.Route.extend({
+  model: function(params){
+    return users[params.user_id];
+  }
+});
+
+App.EditUserRoute = Ember.Route.extend({
+  model: function(params) {
+    return users[params.user_id];
+  },
+
+  events: {
+    save: function(){
+      var user = this.modelFor('editUser');
+      this.transitionTo('user', user);
+    }
+  }
 });
 
 
-Ember.Handlebars.helper('format-date', function(date) {
-  return moment(date).fromNow();
-});
-
-var showdown = new Showdown.converter();
-
-Ember.Handlebars.helper('format-markdown', function(input){
-  return new Handlebars.SafeString(showdown.makeHtml(input));
-});
-
-
-/*var posts = [{
-  id: '1',
-  title: "Rails is Omakase",
-  author: { name: "d2h" },
-  date: new Date('12-27-2012'),
-  excerpt: "There are lots of à la carte software environments in this world. Places where in order to eat, you must first carefully look over the menu of options to order exactly what you want.",
-  body: "I want this for my ORM, I want that for my template language, and let's finish it off with this routing library. Of course, you're going to have to know what you want, and you'll rarely have your horizon expanded if you always order the same thing, but there it is. It's a very popular way of consuming software.\n\nRails is not that. Rails is omakase."
-}, {
-  id: '2',
-  title: "The Parley Letter",
-  author: { name: "d2h" },
-  date: new Date('12-24-2012'),
-  excerpt: "My [appearance on the Ruby Rogues podcast](http://rubyrogues.com/056-rr-david-heinemeier-hansson/) recently came up for discussion again on the private Parley mailing list.",
-  body: "A long list of topics were raised and I took a time to ramble at large about all of them at once. Apologies for not taking the time to be more succinct, but at least each topic has a header so you can skip stuff you don't care about.\n\n### Maintainability\n\nIt's simply not true to say that I don't care about maintainability. I still work on the oldest Rails app in the world."
-}];*/
+var users = [
+  {
+    id: 0,
+    first: 'Ryan',
+    last: 'Florence',
+    avatar: 'http://placehold.it/50x50"'
+  },
+  {
+    id: 1,
+    first: 'Tom',
+    last: 'Dale',
+    avatar: 'http://placehold.it/50x50"'
+  },
+  {
+    id: 2,
+    first: 'Yehuda',
+    last: 'Katz',
+    avatar: 'http://placehold.it/50x50"'
+  }
+];
